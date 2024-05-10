1f17f317d5b79e2e1110890122a6c27cc831ed58
Abirami RJ <146910216+helloiamabirami@users.noreply.github.com>
2024-05-10 18:11:05 +0530

    Delete Notes-Sharing-Web-Application-using-Django-Framework-Kalatheeswaran-4019-NIT--main-main directory

--- Notes-Sharing-Web-Application-using-Django-Framework-Kalatheeswaran-4019-NIT--main-main/Notes-Sharing-Web-Application-using-Django-Framework-Kalatheeswaran-4019-NIT--main-main/README.md
+++
@@ -1,1 +0,0 @@
-# Notes-Sharing-Web-Application-using-Django-Framework-Abirami R J-4019-NIT--main
--- Notes-Sharing-Web-Application-using-Django-Framework-Kalatheeswaran-4019-NIT--main-main/Notes-Sharing-Web-Application-using-Django-Framework-Kalatheeswaran-4019-NIT--main-main/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from hr import views
-urlpatterns = [
-    path('hrdash/',views.hrHome,name='hrdash'),
-    
-]
